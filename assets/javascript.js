4359d2666c13fa799f20124fc3268856a3e235bf
Alan Teixeira <86982421+Alanteixeira29@users.noreply.github.com>
2022-12-21 11:51:15 -0300

    Add files via upload

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,8 +1,9 @@
+
 var result = 0.0
-var num1 = prompt("Qual é o primeiro número?")
+var num1 = prompt("Qual é o primeiro número? (Utilize ponto e não virgula!)")
 num1 = parseFloat(num1)
 var op = prompt("Qual o operador?")
-var num2 = prompt("Qual o segundo número?")
+var num2 = prompt("Qual o segundo número? (Utilize ponto e não virgula!)")
 num2 = parseFloat(num2)
 
 switch( op ){
@@ -14,6 +15,12 @@ switch( op ){
         result = num1 * num2; break;
     case "/" :
         result = num1 / num2; break;
+    case "%" :
+        result = ((num2 / 100) * num1); break;
     default :
-        alert("Operação inválida")
+        alert("Operação inválida"); break;
 }
+
+    alert("Resultado: " + result)
+    alert("Pressione F5 para utilizar a calculadora novamente!")
+    reload(forceReload)
